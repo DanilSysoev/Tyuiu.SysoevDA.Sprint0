35708023b0c8f467f9981e64999d6fc5b8d4500c
SysoevDA <fuzzii2739@gmail.com>
2023-09-30 22:00:23 +0500

    Task6

--- Tyuiu.SysoevDA.Sprint0.Task5.V0/Program.cs
+++ Tyuiu.SysoevDA.Sprint0.Task5.V0/Program.cs
@@ -16,7 +16,7 @@ namespace Tyuiu.SysoevDA.Sprint0.Task5.V0
             Console.WriteLine("A - B = " + DataService.Subtraction(15, 5));
             Console.WriteLine("A * B = " + DataService.Multiplication(10, 10));
 
-            Console.WriteLine("A / B = " + DataService.Division(5, 0));
+            Console.WriteLine("AConsoleApp1 / B = " + DataService.Division(5, 0));
 
             Console.ReadKey();
 
---
+++ Tyuiu.SysoevDA.Sprint0.Task6.V0.Test/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Tyuiu.SysoevDA.Sprint0.Task6.V0.Test")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Tyuiu.SysoevDA.Sprint0.Task6.V0.Test")]
+[assembly: AssemblyCopyright("Copyright ©  2023")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("1d88f866-9ce8-45d8-ab52-76cda9425485")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
